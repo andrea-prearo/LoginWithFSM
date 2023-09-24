eb74eb8d288f4bd4eecbaf2d68e6c098b4c9ffaf
Andrea Prearo <andrea.prearo@gmail.com>
2023-09-24 09:55:42 -0700

    Initial Commit

---
+++ LoginWithFSM/LoginWithFSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithFSMApp.swift
+//  LoginWithFSM
+//
+//  Created by Andrea Prearo on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithFSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
